9b7481312ca0f197b9746a6427ac732810e8f25e
anhdev <info.anhdev@gmail.com>
2025-04-14 22:10:09 +0700

    ss

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -10,7 +10,7 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-const uri = process.env.MONGODB_URI;
+// const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client;
@@ -18,12 +18,18 @@ let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(
+      "mongodb+srv://jackrover889:S4fVgxgiliiYH9M6@cluster0.phb1imj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      options
+    );
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(
+    "mongodb+srv://jackrover889:S4fVgxgiliiYH9M6@cluster0.phb1imj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    options
+  );
   clientPromise = client.connect();
 }
 
